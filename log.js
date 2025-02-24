7b8fff99458aead31424cf4109cb03712e780f21
r2d2web <132074583+r2d2web@users.noreply.github.com>
2025-02-24 09:07:35 +0000

    Update log.js

--- log.js
+++ log.js
@@ -1,11 +1,17 @@
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
-  });
+});
   document.addEventListener('keydown', function(event) {
     if (event.ctrlKey) {
         event.preventDefault(); // Disable the default action when Ctrl is pressed
     }
 });
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'F12') {
+        event.preventDefault(); // Disable F12 (DevTools shortcut)
+    }
+});
+
 
 
 function loginUser() {
